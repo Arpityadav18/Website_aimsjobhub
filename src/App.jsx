9ccca63eab3b348ea16081ceb771d33b2eaed317
Arpit Yadav <112938376+Arpityadav18@users.noreply.github.com>
2024-09-23 16:41:31 +0530

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import Home from './components/Home/Home'
+// import Navbar from './components/Navbar/Navbar'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+function App() {
+
+
+  return (
+    <>
+    {/* <Navbar></Navbar> */}
+    <Header/>
+    <Home/>
+    <Footer></Footer>
+    </>
+  )
+}
+
+export default App
