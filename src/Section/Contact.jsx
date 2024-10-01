184fc2d95fac59fefb46baf5e8cc3fe84c07aa89
Arpit Yadav <112938376+Arpityadav18@users.noreply.github.com>
2024-10-01 17:57:51 +0530

    first-commit

--- src/Section/Contact.jsx
+++ src/Section/Contact.jsx
@@ -1,11 +1,17 @@
-import React, { useEffect } from 'react'
+import React, { useEffect ,useState} from 'react'
 import { DarkModeProvider ,useDarkMode} from '../Componets/DarkModeContext'
 import aboutimg from "../assets/images/about.webp"
 import AOS from 'aos';
 import 'aos/dist/aos.css'
-// import GoogleMap from "/src/Section/GoogleMap.jsx"
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+const[profile,setProfile]= useState("");
+  const [message, setMessage] = useState("");
+
+  
 
   useEffect(()=>{
     AOS.init({
@@ -15,104 +21,122 @@ const Contact = () => {
        delay:100,
     });
   },[])
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Here you would typically handle form submission, e.g., send data to a backend
+    console.log({ name, email, mobile,profile, message });
+  };
   const {darkMode, toggleDarkMode} = useDarkMode();
   return (
     <div className={`${darkMode? 'dark bg-black':'light bg-transparent'}`}>
       <section className={`${darkMode? 'dark bg-gray-800':'light bg-purple-100'} w-[90%] h-screen m-auto lg:px-40 px-10 py-20 grid lg:grid-cols-2 grid-cols-1 justify-center items-center gap-10 rounded-xl`}>
       <div className='flex flex-col items-start justify-center gap-8'>
         <h1 className='font-semibold text-xl text-blue-900'>GET IN CONNECT</h1>
-       
-{/*        
-        <div className="grid grid-cols-4">
-        <label htmlFor="email">YOUR EMAIL </label>
-        
-        <div className="mt-2">
-          <input id = "email"
-           name="email"
-                  type="email"
-                  autoComplete="email"
+        <form onSubmit={handleSubmit}>
+        <div data-aos = 'zoom-in' className="flex flex-row">
+          <div data-aos = 'zoom-in' className=" w-full">
+            <label
+              htmlFor="name"
+              className="block my-2 text-left text-sm font-medium text-gray-900 "
+            >
+              Full Name
+            </label>
+            <input data-aos = 'zoom-in'
+              type="text"
+              id="name"
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+              placeholder=" Your Name"
+              className="w-full p-2 border border-gray-300 rounded"
+              required
+            />
+          </div>
+          
+          <div data-aos = 'zoom-in' className="w-full pl-3">
+            <label
+              htmlFor="email"
+              className="block my-2 text-left text-sm font-medium text-gray-900"
+            >
+              Email Address
+            </label>
+            <input data-aos = 'zoom-in'
+              type="email"
+              id="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+              placeholder="Your Email "
+              className="w-full p-2 border border-gray-300 rounded"
+              required
+            />
+          </div>
+        </div>
+        <div data-aos = 'zoom-in' className="flex flex-col ">
+          <div   data-aos = 'zoom-in' className="w-full pr-2">
+            <label
+              htmlFor="phone"
+              className="block my-2 text-left text-sm font-medium text-gray-900"
+            >
+              Phone Number
+            </label>
+            <input data-aos = 'zoom-in'
+              type="tel"
+              id="phone"
+              value={phone}
+              onChange={(event) => setPhone(event.target.value)}
+              placeholder="Phone"
+              className="w-full p-2 border border-gray-300 rounded"
+              required
+            />
+          </div>
 
-                  placeholder=' email here..'
- />
- </div>
-        </div> */}
-{/* 
-        <div className="grid grid-cols-4">
-     <label htmlFor="first-name">FIRST NAME</label>
-     <div className="sm:col-span-3">
-      <input type="text" placeholder='First Name'
-     
-      /> 
-     </div>
+          <div data-aos = 'zoom-in' className="w-full pr-2">
+            <label 
+              htmlFor="profile"
+              className="block my-2 text-left text-sm font-medium text-gray-900"
+            >
+             profile
+            </label>
+            <input data-aos = 'zoom-in'
+              type="text"
+              id="profile"
+              value={profile}
+              onChange={(event) => setProfile(event.target.value)}
+              placeholder="profile"
+              className="w-full p-2 border border-gray-300 rounded"
+              required
+            />
+          </div>
+          <div data-aos = 'zoom-in' className="w-full ">
+            <label
+              htmlFor="message"
+              className="block my-2 text-left text-sm font-medium text-gray-900"
+            >
+              Message
+            </label>
+            <textarea 
+              id="message"
+              value={message}
+              onChange={(event) => setMessage(event.target.value)}
+              placeholder="Message"
+              rows="4"
+              className="w-full p-2 border border-gray-300 rounded"
+              required
+            />
+          </div>
+        </div>
+        <button 
+        type = 'Submit'
+           className="mt-2 p-2 float-right text-white rounded-lg border-green-600 bg-green-600 hover:scale-105"
+         
+        >
+          Send message
+        </button>
+      </form>
+      
+
       </div>
-    */}
-    
-      {/* <div className="grid grid-cols-4">
-              <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">
-                Last name
-              </label>
-              <div className="mt-2">
-                <input
-                  id="last-name"
-                  name="last-name"
-                  type="text"
-                  placeholder='last-name'
-                  autoComplete="family-name"
-                 
-                />
-              </div>
-            </div>
-         */}
-
-
-
-{/* for resume upload */}
-
-
-{/* <div className=" grid grid-cols-4">
-  <label htmlFor="resume-file">UPLOAD YOUR RESUME</label>
-  <div className='mt-2'>
-    <input type="file"
-    id = "resume-file" 
-    placeholder='updload file'
-   
-     />
-  </div>
-</div> */}
-{/* // mobile no  */}
-{/* <div>
-  <label htmlFor="phone-no">YOUR PHONE NO</label>
-  <div>
-    <input type="phoneno" id = "phone-no" placeholder='enter your number' 
-    />
-  </div>
-</div> */}
-
- {/* about yourself */}
-{/* 
- <div className="grid grid-cols-4">
-              <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
-                About
-              </label>
-              <div className="mt-2">
-                <textarea
-                  id="about"
-                  name="about"
-                  rows={3}
-                 
-                  defaultValue={''}
-                />
-              </div>
-              <p >Write a few sentences about yourself.</p>
-            </div> */}
-
-
-{/*   
-<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d58882.02683757931!2d75.84073738200752!3d22.723532493238114!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1727438488282!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">LOCATION</iframe> */}
-
-
-
-
+      <div>
+      <iframe data-aos = "zoom-in" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d117757.61092368203!2d75.80095728906251!3d22.73101617733822!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39630389b87158a5%3A0xd6b3f098df4f4d64!2sAIMS%20JOB%20HUB%20Placement!5e0!3m2!1sen!2sin!4v1727774989585!5m2!1sen!2sin" width="530" height="400" style={{ border: 0 }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </div>
       </section>
     
--- src/Section/Footer.jsx
+++ src/Section/Footer.jsx
@@ -1,10 +1,71 @@
 import React from 'react'
-
+import { useDarkMode } from '../Componets/DarkModeContext'
+import { FaInstagram,FaFacebook,FaTwitter,FaFax,FaMobile } from 'react-icons/fa'
+import { Link } from 'react-scroll'
+import { IoMdMail } from 'react-icons/io'
 const Footer = () => {
+  
+  const {darkMode, toggleDarkMode} = useDarkMode();
   return (
-    <div>
-      footer
-    </div>
+   <>
+   <footer className={`${darkMode? 'dark bg-black':'light bg-gray-400'} w-full m-auto lg:px-20 px-10 py-20 grid lg:grid-cols-3 grid-cols-1 justify-center items-start lg:gap-20 gap-10`} >
+  
+<div className='flex flex-col justify-center items-start gap-5'>
+  <h1 className='text-white text-2xl font-semibold'>ABOUT US</h1>
+  <p className='text-slate-200 text-justify'>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam, similique amet 
+    nostrum nisi minima autem quas libero fugit a voluptatem unde eius tenetur impedit possimus quo, totam expedita molestiae exercitationem 
+    praesentium nulla tempora porro incidunt odit. Harum illum similique vel sed cum, saepe tempora tempore mollitia eveniet architecto
+     voluptatum maxime.
+  </p>
+ <div  id = 'social-icons'className='flex justify-start items-center gap-4 mt-4'>
+ <div className='p-3 rounded-xl bg-white hover:bg-red-500 hover:text-white cursor-pointer transform hover:scale-110 transition-transform duration-300'>
+  <FaFacebook className='size-5'/>
+
+ </div>
+
+ <div className='p-3 rounded-xl bg-white hover:bg-red-500 hover:text-white cursor-pointer transform hover:scale-110 transition-transform duration-300' >
+  <FaInstagram className='size-5'/>
+ </div>
+
+
+ <div className='p-3 rounded-xl bg-white hover:bg-red-500 hover:text-white cursor-pointer transform hover:scale-110 transition-transform duration-300' >
+  <FaTwitter  className='size-5' />
+  
+ </div>
+ </div>
+ <h1 className='text-white mt-8'>Copyright AIMS JOB HUB,All Rights Reserved</h1>
+</div>
+ <div className='flex flex-col justify-center items-start gap-5'>
+  <h1 className='text-white text-2xl font-semibold'>Contact Us</h1>
+ 
+  <div  className='flex  justify-center items-start gap-3'>
+    <IoMdMail className='text-white size-5'/>
+      <p className='text-slate-200'>
+      hr.aimsjobhub@gmail.com
+      </p>
+
+  </div>
+  <div  className='flex  justify-center items-start gap-3'>
+    <FaMobile className='text-white size-5'/>
+      <p className='text-slate-200'>
+      8085809797
+      </p>
+
+  </div>
+
+
+  <div  className='flex  justify-center items-start gap-3'>
+    <FaFax className='text-white size-5'/>
+      <p className='text-slate-200'>
+      8085809797
+      </p>
+
+  </div>
+
+ </div>
+   </footer>
+   </>
   )
 }
 
