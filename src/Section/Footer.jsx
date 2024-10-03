37e1da92f9664c70f5c1085cd3dd3d2c56f003eb
Arpit Yadav <112938376+Arpityadav18@users.noreply.github.com>
2024-10-03 17:51:45 +0530

    first-commit

--- src/Section/Footer.jsx
+++ src/Section/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useDarkMode } from '../Componets/DarkModeContext'
-import { FaInstagram,FaFacebook,FaTwitter,FaFax,FaMobile } from 'react-icons/fa'
+import { FaInstagram,FaFacebook,FaTwitter,FaFax,FaMobile, FaLinkedin } from 'react-icons/fa'
 import { Link } from 'react-scroll'
 import { IoMdMail } from 'react-icons/io'
 const Footer = () => {
@@ -8,33 +8,32 @@ const Footer = () => {
   const {darkMode, toggleDarkMode} = useDarkMode();
   return (
    <>
-   <footer className={`${darkMode? 'dark bg-black':'light bg-gray-400'} w-full m-auto lg:px-20 px-10 py-20 grid lg:grid-cols-3 grid-cols-1 justify-center items-start lg:gap-20 gap-10`} >
+   <footer className={`${darkMode? 'dark bg-black':'light bg-gray-700'} w-full m-auto lg:px-20 px-10 py-20 grid lg:grid-cols-3 grid-cols-1 justify-center items-start lg:gap-20 gap-10`} >
   
 <div className='flex flex-col justify-center items-start gap-5'>
-  <h1 className='text-white text-2xl font-semibold'>ABOUT US</h1>
+  <h1 className='text-white text-2xl font-semibold'>AIMS JOB HUB</h1>
   <p className='text-slate-200 text-justify'>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam, similique amet 
-    nostrum nisi minima autem quas libero fugit a voluptatem unde eius tenetur impedit possimus quo, totam expedita molestiae exercitationem 
-    praesentium nulla tempora porro incidunt odit. Harum illum similique vel sed cum, saepe tempora tempore mollitia eveniet architecto
-     voluptatum maxime.
+  114,Orbit mall vijay Nagar , Indore 452010
   </p>
  <div  id = 'social-icons'className='flex justify-start items-center gap-4 mt-4'>
  <div className='p-3 rounded-xl bg-white hover:bg-red-500 hover:text-white cursor-pointer transform hover:scale-110 transition-transform duration-300'>
-  <FaFacebook className='size-5'/>
+  <FaFacebook target = "https://www.facebook.com/AIMSJOBHUB/" className='size-5'/>
 
  </div>
 
  <div className='p-3 rounded-xl bg-white hover:bg-red-500 hover:text-white cursor-pointer transform hover:scale-110 transition-transform duration-300' >
-  <FaInstagram className='size-5'/>
+  <FaInstagram target = "https://www.instagram.com/aimsjobhub/"className='size-5'/>
  </div>
 
 
  <div className='p-3 rounded-xl bg-white hover:bg-red-500 hover:text-white cursor-pointer transform hover:scale-110 transition-transform duration-300' >
-  <FaTwitter  className='size-5' />
-  
+  <FaLinkedin target = "https://www.linkedin.com/in/arpit-yadav-0026b7294/?originalSubdomain=in" className='size-5' />
+    
+{/* 
+<FaLinkedin href="https://www.linkedin.com/in/arpit-yadav-0026b7294/?originalSubdomain=in" className="text-xl" target="_blank" rel="noopener noreferrer" /> */}
  </div>
  </div>
- <h1 className='text-white mt-8'>Copyright AIMS JOB HUB,All Rights Reserved</h1>
+ {/* <h1 className='text-white mt-8'>Copyright AIMS JOB HUB,All Rights Reserved</h1> */}
 </div>
  <div className='flex flex-col justify-center items-start gap-5'>
   <h1 className='text-white text-2xl font-semibold'>Contact Us</h1>
@@ -62,7 +61,74 @@ const Footer = () => {
       </p>
 
   </div>
+  <div  className='flex  justify-center items-start gap-3'>
+   
+      <p className='text-slate-200'>
+        Director - HRD ARPIT YADAV
+      </p>
 
+  </div>
+
+  <div  className='flex  justify-center items-start gap-3'>
+   
+   <p className='text-slate-200'>
+     Director - HRD TANIYA YADAV
+   </p>
+
+   
+
+</div>
+</div>
+ <div className='flex flex-col justify-center items-start gap-5'>
+  <h1 className='text-white text-2xl font-semibold'>OUR SERVICES</h1>
+ 
+  <div  className='flex  justify-center items-start gap-3'>
+   
+      <p className='text-slate-200'>
+      HAND TO HAND PLACEMENT
+      </p>
+
+  </div>
+  <div  className='flex  justify-center items-start gap-3'>
+   
+      <p className='text-slate-200'>
+    PLACEMENT GUARANTEE 100 PERCENT
+      </p>
+
+  </div>
+
+
+  <div  className='flex  justify-center items-start gap-3'>
+   
+      <p className='text-slate-200'>
+   PERMANENT PLACEMENT
+      </p>
+
+  </div>
+  <div  className='flex  justify-center items-start gap-3'> 
+   
+      <p className='text-slate-200'>
+     200+ CLIENTS COMPANIES
+      </p>
+
+  </div>
+
+ <div  className='flex  justify-center items-start gap-3'>
+   
+   <p className='text-slate-200'>
+    HAPPY CANDIDATES
+   </p>
+
+   
+
+</div> 
+
+{/* <div className='flex flex-col justify-center items-center gap-5'>
+<h1 className='text-white mt-8'>Copyright AIMS JOB HUB,All Rights Reserved</h1>
+</div> */}
+<div className="text-center text-gray-500 text-sm p-4">
+  &copy; {new Date().getFullYear()} AIMS JOB HUB. All rights reserved.
+</div>
  </div>
    </footer>
    </>
--- src/Section/Header.jsx
+++ src/Section/Header.jsx
@@ -1,10 +1,55 @@
 import React from 'react'
-
+import { useDarkMode } from '../Componets/DarkModeContext'
+import { Link } from 'react-scroll'
+import { FaXmark ,FaBars} from 'react-icons/fa6'
+import logo from "../assets/images/logo.jpeg"
+import { useState } from 'react'
+import { FaPhoneAlt,FaUserCircle } from 'react-icons/fa'
 const Header = () => {
+  const {darkMode, toggleDarkMode} = useDarkMode();
+   const [isMenuOpen ,setMenuOpen] = useState(false)
+   const toggleMenu = ()=>{
+    setMenuOpen(isMenuOpen)
+   }
+
+   const closeMenu = ()=>{
+    setMenuOpen(false)
+   }
+   const navItems =  [
+    {
+      link:'Home',path:'home'
+    },
+    {
+      link:'About',path:'about'
+    },
+     {
+      link:'Service',path:'service'
+    },
+    {
+      link:'Contact',path:'contact'
+    }
+    // , {
+    //   link:'',path:'home'
+    // }
+
+   ]
+
   return (
-    <div>
-      header
-    </div>
+    <>
+    
+  
+    <nav className={`${darkMode? 'dark bg-black':'light bg-[#f3f3f3]'} flex justify-between items-center gap-4 lg:px-20 px-4 py-3 sticky top-0 z-30`}>
+      <div id = 'logo'>
+  
+<img src={logo} alt="company logo" className='lg:w-[150px] w-[120px] dark:invert' />
+<ul className='lg:flex justify-center items-center gap-8 hidden'>
+  {navItems.map(({link,path})=>(
+  <Link key = {path} className='text-black text-[15px] uppercase font-semibold cursor-pointer px-3 py-2 dark:text-white rounded-lg hover:bg-red-600 hover:text-white to={path} spy={true} offset={-100} smooth={true}'>{link}</Link>
+  ))}
+</ul>
+      </div>
+    </nav>
+    </>
   )
 }
 
